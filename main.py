4620f40748c1e1ad425fb21148405491a12d9e1b
AngryMexDog <aortiz19.depad.sj@gmail.com>
2023-08-31 17:14:47 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band generator.")
+city = input("Which city did you grow up in? ")
+pet = input("What is the name of your pet? ")
+print(f"Your name band could be {city} {pet}")
